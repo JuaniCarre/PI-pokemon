ecf37ef98053133f95dbc80f630faf05a38e8719
Juani <juanicarrenio@gmail.com>
2023-03-03 23:52:43 -0300

    funca?

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
     console.log('Server listening at '+ PORT); // eslint-disable-line no-console
   });
 });
